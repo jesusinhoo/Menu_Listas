f7a13bc6db1a854ef7b80b18d0665bf012a647f2
jesusinhoo <jesus.bojorquez1110@gmail.com>
2024-10-15 14:24:14 -0700

    Agregar archivos de proyecto.

---
+++ Menu_Listas/Lista.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu_Listas
+{
+    internal class Lista
+    {
+
+        private Nodo inicio;
+        private int contador;
+
+        public Lista()
+        {
+            inicio = null;
+            contador = 0;
+        }
+
+        public void Add(int num)
+        {
+            Nodo nuevo = new Nodo(num);
+            if (inicio == null)
+            {
+                inicio = nuevo;
+            }
+            else
+            {
+                Nodo actual = inicio;
+                while (actual.Sig != null)
+                {
+                    actual = actual.Sig;
+                }
+                actual.Sig = nuevo;
+            }
+            contador++;
+        }
+
+        public int Count()
+        {
+            return contador;
+        }
+
+        public int Find(int pos)
+        {
+            Nodo act;
+            act = inicio;
+            int poss = 0;
+            if (act == null)
+            {
+                Console.WriteLine("Lista vacia.");
+            }
+            else
+            {
+                while (act != null)
+                {
+                    if (act.Sig != null)
+                    {
+                        act = act.Sig; poss++;
+                        if (poss == pos)
+                        {
+                            return act.Valor;
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public void Imprimir()
+        {
+            if (inicio == null)
+            {
+                Console.WriteLine("La lista esta vacia");
+            }
+            else
+            {
+                Nodo act;
+                act = inicio;
+                while (act != null)
+                {
+                    Console.Write($"{act.Valor}" + " " + "->" + " ");
+                    act = act.Sig;
+                }
+            }
+        }
+
+    }
+}
---
+++ Menu_Listas/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu_Listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista miLista= new Lista();
+            int wh = 5;
+            while (wh == 5)
+            {
+                Console.Clear();
+                Console.WriteLine("===========MENU LISTAS==========");
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Selecciona una opcion: ");
+                Console.WriteLine("1.- Insertar Nodo.");
+                Console.WriteLine("2.- Imprimir Tamaño.");
+                Console.WriteLine("3.- Buscar Nodo.");
+                Console.WriteLine("4.- Borrar Nodo.");
+                Console.WriteLine("5.- Modificar Nodo.");
+                Console.WriteLine("6.- Buscar Valo.");
+                Console.WriteLine("7.- Imprimir Lista.");
+                Console.WriteLine("8.-  Salir");
+                int opc = int.Parse(Console.ReadLine());
+
+                if (opc == 1)
+                {
+                    Console.WriteLine("Agrega un nodo: ");
+                    int nodo = int.Parse(Console.ReadLine());   
+                    miLista.Add(nodo);
+                    //  Console.Clear();
+                }
+                else if (opc == 2)
+                {
+                    Console.WriteLine("El tamaño de la lista es de: " + miLista.Count());
+                    Console.ReadKey();
+                }
+                else if (opc == 3)
+                {
+
+                }
+                else if (opc == 4)
+                {
+
+                }
+                else if (opc == 5)
+                {
+
+                }
+                else if (opc == 6)
+                {
+
+                }
+                else if (opc == 7)
+                {
+                    Console.Clear() ;
+                    miLista.Imprimir(); 
+                    Console.ReadKey();
+                }
+                else if (opc == 8)
+                {
+                    Console.WriteLine("Saliendo...");
+                    wh = 1;
+                }
+            }
+        }
+    }
+}
